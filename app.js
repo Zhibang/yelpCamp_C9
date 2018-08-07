17195751b63f46c8609ff2d4ffd57c7a07e49cba
Zhibang Chen <chenzhibang1988@gmail.com>
2018-08-07 03:13:46 +0000

    configure DATABASEURL in heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 //mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://cchen:cc1234@ds115022.mlab.com:15022/firstyelpcamp");
+//mongoose.connect("mongodb://cchen:cc1234@ds115022.mlab.com:15022/firstyelpcamp");
+mongoose.connect(process.env.DATABASEURL);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
